046a7ba32d6a767caf65c2cff93153a708a32089
Daeki <kimdaeki@gmail.com>
2024-03-06 11:48:20 +0900

    add HTML

---
+++ src/main/java/com/winter/app/board/BoardService.java
@@ -0,0 +1,13 @@
+package com.winter.app.board;
+
+import java.util.List;
+
+import com.winter.app.util.Pager;
+
+public interface BoardService {
+	
+	public List<BoardVO> getList(Pager pager)throws Exception;
+	
+	public int add(BoardVO boardVO)throws Exception;
+
+}
