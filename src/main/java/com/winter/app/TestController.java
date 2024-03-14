07fef7b4b365ca18ceebd6028b5d9a07b1f3a653
Daeki <kimdaeki@gmail.com>
2024-03-14 12:01:53 +0900

    web client

--- src/main/java/com/winter/app/TestController.java
+++ src/main/java/com/winter/app/TestController.java
@@ -6,17 +6,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.winter.app.ajax.RestTempleTest;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class TestController {
+	
+	@Autowired
+	private RestTempleTest restTempleTest;
 
 	
 	@GetMapping("/")
-	public String test() {
-		
+	public String test() throws Exception {
+		restTempleTest.flux();
 		return "index";
 	}
 	
